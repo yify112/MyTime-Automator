72aceb2d1364a357c5c856af51752149e8366069
sol-invictus481 <rohith481@gmail.com>
2016-05-08 17:31:49 +0530

    Added batch files

---
+++ internalBatch.bat.bat
@@ -0,0 +1,6 @@
+@echo off
+echo internalBatch.bat
+echo MAKE SURE the .js file and this .bat file are in the SAME FOLDER/directory
+echo Running default command casperjs wipromytimeinternal.js
+casperjs wipromytimeinternal.js
+pause
