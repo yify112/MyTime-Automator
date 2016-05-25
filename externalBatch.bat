08033fb134969238105d8e1f78106c4693078ab3
sol-invictus481 <rohith481@gmail.com>
2016-05-25 22:05:06 +0530

    Addded a horizontal divider(line) to make it easier to distinguish

--- externalBatch.bat
+++ externalBatch.bat
@@ -1,6 +1,7 @@
 @echo off
 echo externalBatch.bat
 echo MAKE SURE the .js file and this .bat file are in the SAME FOLDER/directory
+echo -------------------------------------------------------------------------
 echo Running default command casperjs wipromytimeexternal.js
 casperjs wipromytimeexternal.js
 pause
--- internalBatch.bat
+++ internalBatch.bat
@@ -1,6 +1,7 @@
 @echo off
 echo internalBatch.bat
 echo MAKE SURE the .js file and this .bat file are in the SAME FOLDER/directory
+echo -------------------------------------------------------------------------
 echo Running default command casperjs wipromytimeinternal.js
 casperjs wipromytimeinternal.js
 pause
